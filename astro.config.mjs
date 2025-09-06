eede59ef6a20ae90a609594ab702d8f17ede5dbf
apo2073 <apo2073@outlook.com>
2025-09-06 20:50:07 +0900

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://grassproject.github.io',
+    server: {
+        allowedHosts: ['0.tcp.jp.ngrok.io']
+    }
+});
--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import {defineConfig} from "astro/config";
-
-export default defineConfig({
-    server: {
-        allowedHosts: ['0.tcp.jp.ngrok.io']
-    }
-})
