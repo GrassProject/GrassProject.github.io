b3d406bbb943275a58136ab567fe04cccfab079b
apo2073 <apo2073@outlook.com>
2025-09-06 20:31:18 +0900

    init

---
+++ src/script/DiscordUser.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import qs from 'qs';
+
+const clientId = '1358603949015564449';
+const clientSecret = 's8Tf9GpGwOHxaDcqXaF8NnZzF_GEkduL';
+
+export async function getDiscordUser(code: string, redirectUri: string) {
+    try {
+        const tokenRes = await axios.post(
+            'https://discord.com/api/v10/oauth2/token',
+            qs.stringify({
+                grant_type: 'authorization_code',
+                code: code,
+                redirect_uri: redirectUri,
+                scope: 'identify email openid'
+            }),
+            {
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                auth: { username: clientId, password: clientSecret }
+            }
+        );
+
+        const accessToken = tokenRes.data.access_token;
+
+        const userRes = await axios.get('https://discord.com/api/v10/users/@me', {
+            headers: { Authorization: `Bearer ${accessToken}` }
+        });
+
+        return userRes.data;
+    } catch (err: any) {
+        console.error(err.response?.data || err.message);
+        throw new Error(err.message || 'Discord API Error');
+    }
+}
