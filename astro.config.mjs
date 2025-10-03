ed18c30dc179b7c98fdac1abd3d63d5e1705e1ed
APO2073 <santabes93@gmail.com>
2025-10-03 12:40:28 +0900

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-//import node from '@astrojs/node';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://grassproject.github.io',
     // output: 'server',
     base: '/', 
-    //output: 'server',
-    //adapter: node({
-    //    mode: 'standalone',
-    //}),
+    output: 'static',
+    adapter: node({
+        mode: 'standalone',
+    }),
 });
