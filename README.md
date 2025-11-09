02760fd1b58bf72e039b13dce6ea162bb5491001
apo2073 <apo2073@outlook.com>
2025-11-09 21:13:27 +0900

    😺 Change Deploy Setting

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 🌱GrassProject Web Service 
+
+```npm
+npm install // Install dependencies
+npm run build // Build the project
+npm run deploy // Deploy the project
+```
--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,11 @@ import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores([
+      'build/**',
+      'dist/**',
+      'node_modules/**'
+  ]),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
